1860d7ca933d4e31c8c2e39e0dc8c0ad80b409d2
Misaki Koonce <misaki.koonce@gmail.com>
2020-02-28 09:52:53 -0800

    Add append and prepend methods.

--- linked-list/linkedList.js
+++ linked-list/linkedList.js
@@ -1,1 +1,54 @@
 // doubly link list
+
+class Node {
+    constructor(value, prev, next) {
+        this.value = value
+        this.prev = prev || null
+        this.next = next || null
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = this.tail = null
+    }
+
+    // Add to the end (tail) of the list
+    append(value) {
+        // If the list is empty, create a new Node
+        if(!this.tail) {
+            this.head = this.tail = new Node(value)
+        } else {
+            let oldTail = this.tail
+            this.tail = new Node(value)
+            oldTail.next = this.tail
+            this.tail.prev = oldTail
+        }
+    }
+
+    // Add to the beginning (head) of the list
+    prepend(value) {
+        // If the list is empty, create a new Node
+        if(!this.head) {
+            this.head = this.tail = new Node(value)
+        } else {
+            let oldHead = this.head
+            this.head = new Node(value)
+            oldHead.next = this.head
+            this.head.prev = oldHead
+        }
+    }
+
+    removeTail() {
+
+    }
+
+
+    removeHail() {
+
+    }
+
+    search() {
+
+    }
+}
